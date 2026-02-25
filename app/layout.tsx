3b1e54b036dceba9e9765bb4ba227679ef2190d1
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-02-25 22:40:17 +0500

    Metadata set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,45 @@ import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 import "@/styles/globals.css";
 import { Providers } from "@/providers/provider";
+import { SITE_CONFIG } from "@/lib/constants";
+import { fontVariables } from "@/lib/fonts";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(SITE_CONFIG.url),
+  title: {
+    default: SITE_CONFIG.name,
+    template: `%s | ${SITE_CONFIG.author}`,
+  },
+  description: SITE_CONFIG.description.en,
+  authors: [{ name: SITE_CONFIG.author }],
+  creator: SITE_CONFIG.author,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    alternateLocale: "ru_RU",
+    url: SITE_CONFIG.url,
+    siteName: SITE_CONFIG.name,
+    images: [
+      {
+        url: SITE_CONFIG.ogImage,
+        width: 1200,
+        height: 630,
+        alt: SITE_CONFIG.name,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
 
 export const viewport: Viewport = {
   themeColor: [
