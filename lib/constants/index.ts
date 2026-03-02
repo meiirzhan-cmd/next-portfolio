33af9c3662df5da5b8e820445eb552df04d91e08
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-03-02 20:26:49 +0500

    Add barrel index for lib/constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,14 @@
+export { SITE_CONFIG } from "./site";
+export { STORAGE_KEYS } from "./storage";
+export {
+  LOCALES,
+  DEFAULT_LOCALE,
+  LOCALE_LABELS,
+  LOCALE_HTML_LANG,
+  LOCALE_OG,
+} from "./locale";
+export type { Locale } from "./locale";
+export { NAV_ROUTES } from "./navigation";
+export type { NavRouteKey } from "./navigation";
+export { SOCIAL_PLATFORMS } from "./social";
+export type { SocialPlatform } from "./social";
