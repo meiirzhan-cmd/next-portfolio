938e37e8bb7c42c8f4a5cd66861755856adb3b06
Meiirzhan Baitangatov <rikimarubro@gmail.com>
2026-03-02 20:26:29 +0500

    Add storage keys constants to lib/constants/storage.ts

---
+++ lib/constants/storage.ts
@@ -0,0 +1,6 @@
+export const STORAGE_KEYS = {
+  animations: "portfolio-animations-enabled",
+  theme: "portfolio-theme",
+  locale: "portfolio-locale",
+  cookieConsent: "portfolio-cookie-consent",
+} as const;
